09da97dfc0a3a1a62c4db4dd848325211165c45e
emre@rakam.io <emre@rakam.io>
2021-04-27 03:48:40 +0300

    changed dbt_project.yml

---
+++ models/rakam/source_snowflake_tpch_sample_tpch_orders_segmentation_totals_by_date.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+   
+    schema = "RAKAM_AGGREGATES",
+    alias = "SOURCE_SNOWFLAKE_TPCH_SAMPLE_TPCH_ORDERS_SEGMENTATION_TOTALS_BY_DATE",
+    materialized = "incremental",
+    tags = ['rakam_materialize']
+  )
+}}
+SELECT * FROM (SELECT 
+    source_snowflake_tpch_sample_tpch_orders.O_ORDERDATE AS o_orderdate,
+    source_snowflake_tpch_sample_tpch_orders.O_ORDERSTATUS AS o_orderstatus,
+    source_snowflake_tpch_sample_tpch_orders.O_ORDERPRIORITY AS o_orderpriority,
+    count(*) AS total_orders,
+    hll_accumulate(source_snowflake_tpch_sample_tpch_orders.O_CUSTKEY) AS total_users
+FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS AS source_snowflake_tpch_sample_tpch_orders
+
+
+GROUP BY
+    1,  2,  3 
+
+) AS t
+{% if is_incremental() %}
+   WHERE source_snowflake_tpch_sample_tpch_orders.O_ORDERDATE > (select max(o_orderdate) from {{ this }})
+{% endif %}
