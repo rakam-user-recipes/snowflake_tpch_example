846862189464e78b6dd285dfa546fd55b124fff3
emre@rakam.io <emre@rakam.io>
2021-08-23 12:53:48 +0000

    changed models/orders.yml

--- models/metriql/source_snowflake_tpch_sample_tpch_orders_segmentation_totals_by_date.sql
+++ models/metriql/source_snowflake_tpch_sample_tpch_orders_segmentation_totals_by_date.sql
@@ -13,10 +13,10 @@ SELECT * FROM (SELECT
     source_snowflake_tpch_sample_tpch_orders.O_ORDERSTATUS AS o_orderstatus,
     source_snowflake_tpch_sample_tpch_orders.O_ORDERPRIORITY AS o_orderpriority,
     count(*) AS total_orders,
-    hll_accumulate(source_snowflake_tpch_sample_tpch_orders.O_CUSTKEY) AS total_users
+    hll_accumulate((source_snowflake_tpch_sample_tpch_orders.O_CUSTKEY)) AS total_users
 FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS AS source_snowflake_tpch_sample_tpch_orders
 
-GROUP BY
+    GROUP BY
     1,  2,  3 
 ) AS source_snowflake_tpch_sample_tpch_orders
 {% if is_incremental() %}
