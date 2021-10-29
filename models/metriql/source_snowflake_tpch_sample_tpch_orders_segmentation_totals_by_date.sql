3cf1cd768245e13563dd26c5b37dfdad1e9ce077
emre@rakam.io <emre@rakam.io>
2021-10-29 19:51:44 +0000

    changed models/orders.yml

--- models/metriql/source_snowflake_tpch_sample_tpch_orders_segmentation_totals_by_date.sql
+++ models/metriql/source_snowflake_tpch_sample_tpch_orders_segmentation_totals_by_date.sql
@@ -12,12 +12,13 @@ SELECT * FROM (SELECT
     source_snowflake_tpch_sample_tpch_orders.O_ORDERDATE AS o_orderdate,
     source_snowflake_tpch_sample_tpch_orders.O_ORDERSTATUS AS o_orderstatus,
     source_snowflake_tpch_sample_tpch_orders.O_ORDERPRIORITY AS o_orderpriority,
+    source_snowflake_tpch_sample_tpch_customer.C_MKTSEGMENT AS customer_c_mktsegment,
     count(*) AS total_orders,
     hll_accumulate((source_snowflake_tpch_sample_tpch_orders.O_CUSTKEY)) AS total_users
 FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS AS source_snowflake_tpch_sample_tpch_orders
-
+ LEFT JOIN SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER AS source_snowflake_tpch_sample_tpch_customer ON (source_snowflake_tpch_sample_tpch_orders.o_custkey = source_snowflake_tpch_sample_tpch_customer.c_custkey) 
     GROUP BY
-    1,  2,  3 
+    1,  2,  3,  4 
 ) AS source_snowflake_tpch_sample_tpch_orders
 {% if is_incremental() %}
    WHERE source_snowflake_tpch_sample_tpch_orders.O_ORDERDATE > (select max(o_orderdate) from {{ this }})
